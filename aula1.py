643db41c334e931eb60e46623502e33aaeff1cb7
Dannylopes <danielmotalopes83@gmail.com>
2025-05-19 23:27:21 -0300

    commit das strings

---
+++ aula1.py
@@ -0,0 +1,20 @@
+'''
+Isso é um Docstring
+'''
+
+print("Olá Mundo python")
+
+# Tipos de dados
+# Python possui tipagem dinâmica e forte.
+# int
+idade = 20
+# float
+altura = 1.75
+# str
+nome = "João"
+# bool
+maior_de_idade = True
+
+print(nome, "possui", idade, "anos e", altura, "de altura")
+
+print("Daniel \" Mota")
