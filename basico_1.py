a17f06e28fe182d30105ad953adc9c8594c9e5ea
Dannylopes <danielmotalopes83@gmail.com>
2025-05-22 20:44:50 -0300

    arquivo renomeado

--- basico_1.py
+++ basico_1.py
@@ -9,7 +9,7 @@ print("Olá Mundo python")
 # Tipos de dados
 # Python possui tipagem dinâmica e forte.
 # int
-idade = 20
+idade = input("Digite sua idade: ")
 # float
 altura = 1.75
 # str
@@ -44,15 +44,37 @@ print(int('1')) # Aqui digitou-se um string e o python converteu para int
 
 print(float('1.2')) # Aqui digitou-se um string e o python converteu para float
 
-print(bool(1))
+print(bool(1)) #1 é True, 0 é False
 
 import re
 
 # Limpando o input que aceita letras e outros caracteres
+
+
 numero = input("Digite sua idade: ")
 
-numero_limpo = re.sub(r'[^0-9]', '', numero)
 
-resultado = int(numero_limpo)
+# numero_limpo = int(re.sub(r'[^0-9]', '', numero))
+
+resultado = int(numero)
 
 print(nome , 'possui', resultado, 'anos')
+
+
+# Variáveis
+# A PEP8 recomenda que as variáveis sejam escritas em minúsculas, separadas por underline
+nome = input("Digite seu nome: ")
+
+sobrenome = input("Digite seu sobrenome: ")
+
+nome_completo = f"Seu nome completo é {nome} {sobrenome}"
+
+print(nome_completo)
+ # Os valores primitivos de variáveis são int, float, str, bool.
+
+
+print('Nome:', nome, 'Idade:', idade)
+
+
+
+
